de3f62e19679456b5c41c7ab7fd9370112cf5ffb
mark8773 <158565946+mark8773@users.noreply.github.com>
2024-05-26 22:13:39 +0300

    😳😳😳

--- build_wine.sh
+++ build_wine.sh
@@ -397,11 +397,11 @@ if [ "$TERMUX_GLIBC" = "true" ]; then
     patch -d wine -Np1 < "${scriptdir}"/esync.patch && \
 #    echo "Applying address space patch"
 #    patch -d wine -Np1 < "${scriptdir}"/termux-wine-fix-staging.patch && \
-    echo "Applying path change patch"
-    if git -C "${BUILD_DIR}/wine" log | grep -q 4e04b2d5282e4ef769176c94b4b38b5fba006a06; then
-    patch -d wine -Np1 < "${scriptdir}"/pathfix-wine9.5.patch
-    else
-    patch -d wine -Np1 < "${scriptdir}"/pathfix.patch
+#    echo "Applying path change patch"
+#    if git -C "${BUILD_DIR}/wine" log | grep -q 4e04b2d5282e4ef769176c94b4b38b5fba006a06; then
+#    patch -d wine -Np1 < "${scriptdir}"/pathfix-wine9.5.patch
+#    else
+#    patch -d wine -Np1 < "${scriptdir}"/pathfix.patch
     fi || {
         echo "Error: Failed to apply one or more patches."
         exit 1
